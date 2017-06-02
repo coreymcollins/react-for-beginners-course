9dcc2b113234b57dabd4481826e9f2baef45ebfb
Corey Collins <coreymcollins@gmail.com>
2017-06-02 13:29:13 -0500

    Import our styles

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -4,6 +4,9 @@ import React from 'react';
 // Just import one method (render) from react-dom instead of the entire thing.
 import { render } from 'react-dom';
 
+// Import our CSS.
+import './css/style.css';
+
 // Import our StorePicker component.
 import StorePicker from './components/StorePicker';
 
