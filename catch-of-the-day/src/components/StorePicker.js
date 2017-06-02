01a25b76649ccd0fcaa043bde43f0249b2edbc42
Corey Collins <coreymcollins@gmail.com>
2017-06-02 13:21:50 -0500

    Adds a form element to our StorePicker component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -6,7 +6,15 @@ class StorePicker extends React.Component {
 
 	// Create our render method.
 	render() {
-		return <p>Hello</p>
+		// I can still use a comment like normal here.
+		return (
+			<form className="form-selector">
+				{ /* But I need to get freaky deaky when commenting in JSX.  Don't put comments at the top level of a return.  They'll break! */ }
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store →</button>
+			</form>
+		)
 	}
 }
 
