ef43cf74ee85f5f2e69fd5ed38db6d9e2cab2c9a
Corey Collins <coreymcollins@gmail.com>
2017-06-02 13:12:24 -0500

    Imports :allthethings: and renders our StorePicker component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,12 @@
-// let's go!
+// Import React first!
+import React from 'react';
+
+// Just import one method (render) from react-dom instead of the entire thing.
+import { render } from 'react-dom';
+
+// Import our StorePicker component.
+import StorePicker from './components/StorePicker';
+
+// Render a component.
+// <What does it render>, <what does it append to?>
+render( <StorePicker/>, document.querySelector( '#main' ) );
