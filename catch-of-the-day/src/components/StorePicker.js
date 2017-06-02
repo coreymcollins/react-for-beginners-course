fd6510a75b12ea38a2fd4eb79f01517b8e9f02a9
Corey Collins <coreymcollins@gmail.com>
2017-06-02 13:11:58 -0500

    Adds the StorePicker component file

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,14 @@
+// Import React first!
+import React from 'react';
+
+// Create a component.
+class StorePicker extends React.Component {
+
+	// Create our render method.
+	render() {
+		return <p>Hello</p>
+	}
+}
+
+// Export this so it can be used elsewhere.
+export default StorePicker;
