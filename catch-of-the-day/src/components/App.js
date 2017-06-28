bb65707e889ecd939ea58748a8ced57aa7895479
Corey Collins <coreymcollins@gmail.com>
2017-06-28 11:03:38 -0500

    Adds the Add Fish Form from module 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,41 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes.js';
 
 class App extends React.Component {
+
+	constructor() {
+		super();
+
+		this.addFish = this.addFish.bind( this );
+		this.loadSamples = this.loadSamples.bind( this );
+
+		// Get the initial state.
+		this.state = {
+			fishes: {},
+			order: {},
+		};
+	}
+
+	addFish( fish ) {
+		// Update our state.
+		// This copies our current state.
+		const fishes = {...this.state.fishes};
+
+		// Add our new fish.
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+
+		// Set the state.
+		this.setState({ fishes });
+	}
+
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
 
 	render() {
 		return (
@@ -12,7 +45,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
