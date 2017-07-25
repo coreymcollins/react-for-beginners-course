321df8e6f7900168c70393d9ea6903708b0477e4
Corey Collins <coreymcollins@gmail.com>
2017-07-25 11:16:41 -0500

    Saves the order information in local storage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -23,15 +23,31 @@ class App extends React.Component {
 	}
 
 	componentWillMount() {
+
+		// Runs right before the app is rendered.
 		this.ref = base.syncState( `${this.props.params.storeId}/fishes`
 		, {
 			context: this,
 			state: 'fishes',
 		});
+
+		// Check localStorage for orders.
+		const localStorageRef = localStorage.getItem( `order-${this.props.params.storeId}` );
+
+		if ( localStorageRef ) {
+			// Update our App component's order state.
+			this.setState({
+				order: JSON.parse( localStorageRef )
+			});
+		}
 	}
 
 	componentWillUnmount() {
 		base.removeBinding( this.ref );
+	}
+
+	componentWillUpdate( nextProps, nextState ) {
+		localStorage.setItem( `order-${this.props.params.storeId}`, JSON.stringify( nextState.order ) );
 	}
 
 	addFish( fish ) {
@@ -77,7 +93,7 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order} />
+				<Order fishes={this.state.fishes} order={this.state.order} params={this.props.params} />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
